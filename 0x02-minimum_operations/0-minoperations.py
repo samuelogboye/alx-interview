d5ff839d9221e659ecf5d8e1cd71e8e58bc70fa4
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-16 06:12:04 +0100

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations required
+    to reduce a given number to 1.
+    Parameters:
+        n (int): The number to be reduced.
+    Returns:
+        int: The minimum number of operations required.
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    while n > 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n -= 1
+        operations += 1
+    return operations
