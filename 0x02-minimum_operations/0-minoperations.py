dc1b3a140f573d8d0f606d2af345d6129e96b3cc
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-18 21:58:45 +0100

    updated more

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,24 +3,42 @@
 import math
 
 
+from math import isqrt
+
 def factors(n):
-    """factors of n number"""
+    """
+    Calculates the factors of a given number.
+
+    Parameters:
+        n (int): The number for which factors need to be calculated.
+
+    Returns:
+        list: A list of factors of the given number.
+    """
     mylist = []
     while n % 2 == 0:
         mylist.append(2)
-        n = n / 2
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        n //= 2
+    for i in range(3, isqrt(n) + 1, 2):
         while n % i == 0:
             mylist.append(i)
-            n = n / i
+            n //= i
     if n > 2:
         mylist.append(n)
     return mylist
 
 
 def minOperations(n):
-    """calculate the minimum operations"""
-    if type(n) != int or n < 2:
+    """
+    Calculates the minimum number of operations required to transform a given number into 1.
+
+    Parameters:
+    n (int): The number to be transformed.
+
+    Returns:
+    int: The minimum number of operations required.
+    """
+    if not isinstance(n, int) or n < 2:
         return 0
     else:
         numOperations = sum(factors(n))
